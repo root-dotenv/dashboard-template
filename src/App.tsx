671d5ccb2aa6bc50c7169315e8bdc93e987aeabd
root-dotenv <rootdotenv@gmail.com>
2025-09-15 01:39:14 +0300

    - - - 🖌️ custom shadcn CARD, & BADGES components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Button } from "./components/ui/button";
+import { Badge } from "./components/ui/badge";
 
 function App() {
   return (
     <>
-      <div className="w-screen h-screen flex items-center justify-center">
-        <Button>Give No Fxck</Button>
+      <div className="w-screen h-screen flex items-center gap-4 justify-center">
+        <Badge variant={"success"}>success</Badge>
+        <Badge variant={"pending"}>pending</Badge>
+        <Badge variant={"failed"}>failed</Badge>
       </div>
     </>
   );
---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const badgeVariants = cva(
+  "inline-flex items-center justify-center rounded-md border px-2 py-0.5 text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",
+  {
+    variants: {
+      variant: {
+        default:
+          "border-transparent bg-primary text-primary-foreground [a&]:hover:bg-primary/90",
+        secondary:
+          "border-transparent bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90",
+        destructive:
+          "border-transparent bg-destructive text-white [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+        outline:
+          "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+        success:
+          "flex items-center justify-center bg-[#ECFDF3] rounded-full border border-green-200 text-[13px] text-[#039855] font-medium px-[12px] py-[1.75px]",
+        pending:
+          "flex items-center justify-center bg-[#FFFAEB] rounded-full border border-orange-200 text-[13px] text-[#DC6803] font-medium px-[12px] py-[1.75px]",
+        failed:
+          "flex items-center justify-center bg-[#FEF3F2] rounded-full border border-red-200 text-[13px] text-[#D92D20] font-medium px-[12px] py-[1.75px]",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+function Badge({
+  className,
+  variant,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"span"> &
+  VariantProps<typeof badgeVariants> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : "span";
+
+  return (
+    <Comp
+      data-slot="badge"
+      className={cn(badgeVariants({ variant }), className)}
+      {...props}
+    />
+  );
+}
+
+export { Badge, badgeVariants };
